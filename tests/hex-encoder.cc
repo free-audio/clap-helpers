5bfe379f19c2cb519c91d22628efb94f6bbc17e2
Alexandre Bique <bique.alexandre@gmail.com>
2023-01-16 13:03:03 +0100

    More unit tests

--- tests/hex-encoder.cc
+++ tests/hex-encoder.cc
@@ -12,4 +12,18 @@ namespace {
       const std::vector<uint8_t> decoded = clap::helpers::hex_decode(encoded);
       CATCH_CHECK(data == decoded);
    }
+
+   CATCH_TEST_CASE("hex decoder - upper case hex") {
+      const std::vector<uint8_t> data{0xF0, 0xA8, 0xC4, 0x0F, 0xFF};
+      const std::vector<uint8_t> decoded = clap::helpers::hex_decode("F0A8C40FFF");
+      CATCH_CHECK(data == decoded);
+   }
+
+   CATCH_TEST_CASE("hex decoder - bad char") {
+      CATCH_CHECK_THROWS(clap::helpers::hex_decode("yl"));
+   }
+
+   CATCH_TEST_CASE("hex decoder - bad length") {
+      CATCH_CHECK_THROWS(clap::helpers::hex_decode("f3d"));
+   }
 } // namespace
