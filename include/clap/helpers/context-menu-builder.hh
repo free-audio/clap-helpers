13df7ea711ee7a6156099bd5718c2eb4b0190a29
jatin <jatinchowdhury18@gmail.com>
2023-04-17 08:46:02 -0700

    Fixes from PR comments

--- include/clap/helpers/context-menu-builder.hh
+++ include/clap/helpers/context-menu-builder.hh
@@ -6,21 +6,21 @@ namespace clap { namespace helpers {
 
    class ContextMenuBuilder {
    public:
-      ContextMenuBuilder() = default;
-
       // not copyable, not moveable
       ContextMenuBuilder(const ContextMenuBuilder &) = delete;
       ContextMenuBuilder(ContextMenuBuilder &&) = delete;
       ContextMenuBuilder &operator=(const ContextMenuBuilder &) = delete;
       ContextMenuBuilder &operator=(ContextMenuBuilder &&) = delete;
 
-      virtual bool addItem(clap_context_menu_item_kind_t item_kind, const void *item_data) {
-         return false;
-      }
+      virtual bool addItem(clap_context_menu_item_kind_t item_kind, const void *item_data) = 0;
 
-      virtual bool supports(clap_context_menu_item_kind_t item_kind) { return false; }
+      virtual bool supports(clap_context_menu_item_kind_t item_kind) const noexcept = 0;
 
       const clap_context_menu_builder *builder() const noexcept { return &_builder; }
+
+   protected:
+      ContextMenuBuilder() = default;
+      virtual ~ContextMenuBuilder() = default;
 
    private:
       static bool clapAddItem(const struct clap_context_menu_builder *builder,
