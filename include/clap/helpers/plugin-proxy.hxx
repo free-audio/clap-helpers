8d354d92068b8ffdbcebd547ec0dd4789611adc5
trinitou <debuggleburger@gmail.com>
2023-10-14 13:18:18 +0200

    PluginProxy: almost all* methods can be const
    *except init() and destroy()
    + noexcept was missing for guiHide()

--- include/clap/helpers/plugin-proxy.hxx
+++ include/clap/helpers/plugin-proxy.hxx
@@ -25,7 +25,7 @@ namespace clap { namespace helpers {
 
    template <MisbehaviourHandler h, CheckingLevel l>
    template <typename T>
-   void PluginProxy<h, l>::getExtension(const T *&ptr, const char *id) noexcept {
+   void PluginProxy<h, l>::getExtension(const T *&ptr, const char *id) const noexcept {
       assert(!ptr);
       assert(id);
 
@@ -51,29 +51,29 @@ namespace clap { namespace helpers {
    template <MisbehaviourHandler h, CheckingLevel l>
    bool PluginProxy<h, l>::activate(double sampleRate,
                               uint32_t minFramesCount,
-                              uint32_t maxFramesCount) noexcept {
+                              uint32_t maxFramesCount) const noexcept {
       return _plugin.activate(&_plugin, sampleRate, minFramesCount, maxFramesCount);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::deactivate() noexcept { _plugin.deactivate(&_plugin); }
+   void PluginProxy<h, l>::deactivate() const noexcept { _plugin.deactivate(&_plugin); }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::startProcessing() noexcept { return _plugin.start_processing(&_plugin); }
+   bool PluginProxy<h, l>::startProcessing() const noexcept { return _plugin.start_processing(&_plugin); }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::stopProcessing() noexcept { _plugin.stop_processing(&_plugin); }
+   void PluginProxy<h, l>::stopProcessing() const noexcept { _plugin.stop_processing(&_plugin); }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::reset() noexcept { _plugin.reset(&_plugin); }
+   void PluginProxy<h, l>::reset() const noexcept { _plugin.reset(&_plugin); }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   clap_process_status PluginProxy<h, l>::process(const clap_process_t *process) noexcept {
+   clap_process_status PluginProxy<h, l>::process(const clap_process_t *process) const noexcept {
       return _plugin.process(&_plugin, process);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::onMainThread() noexcept { _plugin.on_main_thread(&_plugin); }
+   void PluginProxy<h, l>::onMainThread() const noexcept { _plugin.on_main_thread(&_plugin); }
 
    /////////////////////////////
    // clap_plugin_audio_ports //
@@ -91,7 +91,7 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   uint32_t PluginProxy<h, l>::audioPortsCount(bool isInput) noexcept {
+   uint32_t PluginProxy<h, l>::audioPortsCount(bool isInput) const noexcept {
       assert(canUseAudioPorts());
       ensureMainThread("audio_ports.count");
       return _pluginAudioPorts->count(&_plugin, isInput);
@@ -100,7 +100,7 @@ namespace clap { namespace helpers {
    template <MisbehaviourHandler h, CheckingLevel l>
    bool PluginProxy<h, l>::audioPortsGet(uint32_t index,
                                    bool isInput,
-                                   clap_audio_port_info_t *info) noexcept {
+                                   clap_audio_port_info_t *info) const noexcept {
       assert(canUseAudioPorts());
       ensureMainThread("audio_ports.get");
       return _pluginAudioPorts->get(&_plugin, index, isInput, info);
@@ -126,105 +126,105 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiIsApiSupported(const char *api, bool isFloating) noexcept {
+   bool PluginProxy<h, l>::guiIsApiSupported(const char *api, bool isFloating) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.is_api_supported");
       return _pluginGui->is_api_supported(&_plugin, api, isFloating);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiGetPreferredApi(const char **api, bool *isFloating) noexcept {
+   bool PluginProxy<h, l>::guiGetPreferredApi(const char **api, bool *isFloating) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.get_preferred_api");
       return _pluginGui->get_preferred_api(&_plugin, api, isFloating);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiCreate(const char *api, bool isFloating) noexcept {
+   bool PluginProxy<h, l>::guiCreate(const char *api, bool isFloating) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.create");
       return _pluginGui->create(&_plugin, api, isFloating);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::guiDestroy() noexcept {
+   void PluginProxy<h, l>::guiDestroy() const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.destroy");
       _pluginGui->destroy(&_plugin);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiSetScale(double scale) noexcept {
+   bool PluginProxy<h, l>::guiSetScale(double scale) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.set_scale");
       return _pluginGui->set_scale(&_plugin, scale);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiGetSize(uint32_t *width, uint32_t *height) noexcept {
+   bool PluginProxy<h, l>::guiGetSize(uint32_t *width, uint32_t *height) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.get_size");
       return _pluginGui->get_size(&_plugin, width, height);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiCanResize() noexcept {
+   bool PluginProxy<h, l>::guiCanResize() const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.can_resize");
       return _pluginGui->can_resize(&_plugin);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiGetResizeHints(clap_gui_resize_hints_t *hints) noexcept {
+   bool PluginProxy<h, l>::guiGetResizeHints(clap_gui_resize_hints_t *hints) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.get_resize_hints");
       return _pluginGui->get_resize_hints(&_plugin, hints);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiAdjustSize(uint32_t *width, uint32_t *height) noexcept {
+   bool PluginProxy<h, l>::guiAdjustSize(uint32_t *width, uint32_t *height) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.adjust_size");
       return _pluginGui->adjust_size(&_plugin, width, height);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiSetSize(uint32_t width, uint32_t height) noexcept {
+   bool PluginProxy<h, l>::guiSetSize(uint32_t width, uint32_t height) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.set_size");
       return _pluginGui->set_size(&_plugin, width, height);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiSetParent(const clap_window_t *window) noexcept {
+   bool PluginProxy<h, l>::guiSetParent(const clap_window_t *window) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.set_parent");
       return _pluginGui->set_parent(&_plugin, window);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiSetTransient(const clap_window_t *window) noexcept {
+   bool PluginProxy<h, l>::guiSetTransient(const clap_window_t *window) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.set_transient");
       return _pluginGui->set_transient(&_plugin, window);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::guiSuggestTitle(const char *title) noexcept {
+   void PluginProxy<h, l>::guiSuggestTitle(const char *title) const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.suggest_title");
       _pluginGui->suggest_title(&_plugin, title);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiShow() noexcept {
+   bool PluginProxy<h, l>::guiShow() const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.show");
       return _pluginGui->show(&_plugin);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::guiHide() {
+   bool PluginProxy<h, l>::guiHide() const noexcept {
       assert(canUseGui());
       ensureMainThread("gui.hide");
       return _pluginGui->hide(&_plugin);
@@ -251,21 +251,21 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   uint32_t PluginProxy<h, l>::paramsCount() noexcept {
+   uint32_t PluginProxy<h, l>::paramsCount() const noexcept {
       assert(canUseParams());
       ensureMainThread("params.count");
       return _pluginParams->count(&_plugin);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::paramsGetInfo(uint32_t paramIndex, clap_param_info_t *paramInfo) noexcept {
+   bool PluginProxy<h, l>::paramsGetInfo(uint32_t paramIndex, clap_param_info_t *paramInfo) const noexcept {
       assert(canUseParams());
       ensureMainThread("params.get_info");
       return _pluginParams->get_info(&_plugin, paramIndex, paramInfo);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::paramsGetValue(clap_id paramId, double *outValue) noexcept {
+   bool PluginProxy<h, l>::paramsGetValue(clap_id paramId, double *outValue) const noexcept {
       assert(canUseParams());
       ensureMainThread("params.get_value");
       return _pluginParams->get_value(&_plugin, paramId, outValue);
@@ -275,7 +275,7 @@ namespace clap { namespace helpers {
    bool PluginProxy<h, l>::paramsValueToText(clap_id paramId,
                                        double value,
                                        char *outBuffer,
-                                       uint32_t outBufferCapacity) noexcept {
+                                       uint32_t outBufferCapacity) const noexcept {
       assert(canUseParams());
       ensureMainThread("params.value_to_text");
       return _pluginParams->value_to_text(&_plugin, paramId, value, outBuffer, outBufferCapacity);
@@ -284,7 +284,7 @@ namespace clap { namespace helpers {
    template <MisbehaviourHandler h, CheckingLevel l>
    bool PluginProxy<h, l>::paramsTextToValue(clap_id paramId,
                                        const char *paramValueText,
-                                       double *outValue) noexcept {
+                                       double *outValue) const noexcept {
       assert(canUseParams());
       ensureMainThread("params.text_to_value");
       return _pluginParams->text_to_value(&_plugin, paramId, paramValueText, outValue);
@@ -292,7 +292,7 @@ namespace clap { namespace helpers {
 
    template <MisbehaviourHandler h, CheckingLevel l>
    void PluginProxy<h, l>::paramsFlush(const clap_input_events_t *in,
-                                 const clap_output_events_t *out) noexcept {
+                                 const clap_output_events_t *out) const noexcept {
       assert(canUseParams());
       assert(false); // TODO [active ? audio-thread : main-thread]
       _pluginParams->flush(&_plugin, in, out);
@@ -314,7 +314,7 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::posixFdSupportOnFd(int fd, clap_posix_fd_flags_t flags) noexcept {
+   void PluginProxy<h, l>::posixFdSupportOnFd(int fd, clap_posix_fd_flags_t flags) const noexcept {
       assert(canUsePosixFdSupport());
       ensureMainThread("posix_fd_support.on_fd");
       _pluginPosixFdSupport->on_fd(&_plugin, fd, flags);
@@ -338,7 +338,7 @@ namespace clap { namespace helpers {
    template <MisbehaviourHandler h, CheckingLevel l>
    bool PluginProxy<h, l>::presetLoadFromLocation(uint32_t locationKind,
                                                   const char *location,
-                                                  const char *loadKey) noexcept {
+                                                  const char *loadKey) const noexcept {
       assert(canUsePresetLoad());
       ensureMainThread("preset_load.from_location");
       return _pluginPresetLoad->from_location(&_plugin, locationKind, location, loadKey);
@@ -360,7 +360,7 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   uint32_t PluginProxy<h, l>::remoteControlsCount() noexcept {
+   uint32_t PluginProxy<h, l>::remoteControlsCount() const noexcept {
       assert(canUseRemoteControls());
       ensureMainThread("remote_controls.count");
       return _pluginRemoteControlsState->count(&_plugin);
@@ -368,7 +368,7 @@ namespace clap { namespace helpers {
 
    template <MisbehaviourHandler h, CheckingLevel l>
    bool PluginProxy<h, l>::remoteControlsGet(uint32_t                     pageIndex,
-                                             clap_remote_controls_page_t *page) noexcept {
+                                             clap_remote_controls_page_t *page) const noexcept {
       assert(canUseRemoteControls());
       ensureMainThread("remote_controls.get");
       return _pluginRemoteControlsState->get(&_plugin, pageIndex, page);
@@ -390,14 +390,14 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::stateSave(const clap_ostream *stream) noexcept {
+   bool PluginProxy<h, l>::stateSave(const clap_ostream *stream) const noexcept {
       assert(canUseState());
       ensureMainThread("state.save");
       return _pluginState->save(&_plugin, taskIndex);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   bool PluginProxy<h, l>::stateLoad(const clap_istream *stream) noexcept {
+   bool PluginProxy<h, l>::stateLoad(const clap_istream *stream) const noexcept {
       assert(canUseState());
       ensureMainThread("state.load");
       return _pluginState->load(&_plugin, taskIndex);
@@ -419,7 +419,7 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::threadPoolExec(uint32_t taskIndex) noexcept {
+   void PluginProxy<h, l>::threadPoolExec(uint32_t taskIndex) const noexcept {
       assert(canUseThreadPool());
       _pluginThreadPool->exec(&_plugin, taskIndex);
    }
@@ -440,7 +440,7 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void PluginProxy<h, l>::timerSupportOnTimer(clap_id timerId) noexcept {
+   void PluginProxy<h, l>::timerSupportOnTimer(clap_id timerId) const noexcept {
       assert(canUseTimerSupport());
       ensureMainThread("timer_support.on_timer");
       _pluginTimerSupport->on_timer(&_plugin, timerId);
