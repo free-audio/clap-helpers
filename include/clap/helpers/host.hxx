2851221312e1913402c37a6b315481597fe6aee9
trinitou <debuggleburger@gmail.com>
2023-11-23 09:59:58 +0100

    Move CLAP_HELPERS_UNLIKELY to separate header

--- include/clap/helpers/host.hxx
+++ include/clap/helpers/host.hxx
@@ -2,12 +2,7 @@
 #include <iostream>
 
 #include "host.hh"
-
-#if defined(__cplusplus) && __cplusplus >= 202002L
-#   define CLAP_HELPERS_UNLIKELY [[unlikely]]
-#else
-#   define CLAP_HELPERS_UNLIKELY
-#endif
+#include "macros.hh"
 
 namespace clap { namespace helpers {
 
