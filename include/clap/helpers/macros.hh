4dc94a943151493a4862d8a055762d4f02fa2512
Alexandre Bique <bique.alexandre@gmail.com>
2024-11-23 02:04:36 +0100

    Add an utility macro for wrapping exception throw

--- include/clap/helpers/macros.hh
+++ include/clap/helpers/macros.hh
@@ -5,3 +5,9 @@
 #else
 #   define CLAP_HELPERS_UNLIKELY
 #endif
+
+#if defined(__cpp_exceptions)
+#   define CLAP_HELPERS_THROW(X) throw X
+#else
+#   define CLAP_HELPERS_THROW(X) std::terminate()
+#endif
